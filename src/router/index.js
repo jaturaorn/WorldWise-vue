e8d40ecbfe20b10159273095e26334bea4ccd764
jaturaporn <83279262+jaturaorn@users.noreply.github.com>
2026-06-15 12:20:43 +0700

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,18 @@
+import { createWebHistory, createRouter } from "vue-router";
+
+import LoginView from "../views/Login.vue";
+import PageNotFoundView from "../views/PageNotFound.vue";
+import PricingView from "../views/Pricing.vue";
+import ProductView from "../views/Product.vue";
+
+const routes = [
+  { path: "/login", component: LoginView },
+  { path: "/pagenotfound", component: PageNotFoundView },
+  { path: "/pricing", component: PricingView },
+  { path: "/product", component: ProductView },
+];
+
+export const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
---
+++ src/views/AppLayout.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+
+<template>
+  <h1>Welcome to WorldWise!</h1>
+  <p>
+    Explore the world and learn about different countries with our interactive
+    map. Click on a country to discover interesting facts and information.
+  </p>
+</template>
+
+<style scoped></style>
