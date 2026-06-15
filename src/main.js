ae879c18eaa8bd4831a26ec983b58140d7a4f9a4
jaturaporn <83279262+jaturaorn@users.noreply.github.com>
2026-06-15 12:35:24 +0700

    setup pinia

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import { router } from "./router/index.js";
+import createPinia from "pinia";
+
+const pinia = createPinia();
 
 createApp(App).use(router).mount("#app");
